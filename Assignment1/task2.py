693d8133298012d6828052113e3b460ac7267e34
Sachin Kumar Jangir <127860904+SachinJangirX@users.noreply.github.com>
2025-08-24 20:10:37 +0530

    Initial commit - Added task1.py and task2.py

---
+++ Assignment1/task2.py
@@ -0,0 +1,5 @@
+a = str(input('Enter your first name: '))
+b = str(input('Enter your last name: '))
+
+full_name = a + " " + b
+print('Hello,', full_name,'! Welcome to the Python Program.')
---
+++ Basic Python Concepts.py
@@ -0,0 +1,72 @@
+# print("Hello World!")
+# print("Item\tQty\n\nApples\t5\n\nPlums\t10")
+# print(('I am a polite Person\n') * 3) 
+# 1 - integer
+# 2.3 - float
+# 0/1 - boolean
+# 1+1 = 2 (integer addition)
+# '1'+'1' = '11' (string concatination)
+# '1'+1 = error (cannot add string to an integer)
+# 'hi'*3 = 'hihihi' (multiplication of string with int is possible)
+# 'python' * '12' = error (cannot multiply two strings)
+
+
+
+
+# VARIABLES - reserved memory locations
+
+# a=1 variable name a with the value of varaiable = 1
+    # can change the variable value by reassigning the value
+    # 'del a' is used to delete the variable  
+    # can perform multiple operations with multiple variable names and store the result in new variable name 
+    # cant use number for a variable name
+    # can start with _a
+    
+
+
+# INCREAMENT/DECREAMENT 
+
+# a = 3    a = a+1 --> increament or a += 1
+# b = b-1 --> decreament or b -= 1
+# c = c*2 --> multiplication c *= 2
+# d = 3   d /= 2  --> division
+
+
+
+# CODING CHALLENGE 
+
+# c= float(input('Celcius: '))        # taking inputs for the celcius value 
+# f=(c * 9/5)+32              # converting to farenheit nd store in value f
+# print('Farenheit: ', f)         # print the farenheit value of temperature
+
+
+# similarly for farenheit to celcius conversion
+
+
+# f = take input 
+# c = (f-32)*5/9
+# print(c)
+
+
+# SIMPLE INTEREST CALCULATOR 
+
+# Pnr/100
+
+
+# p = float(input('P: '))
+# n = float(input('n: '))
+# r = float(input('r: '))
+# print((p*n*r)/100)
+
+
+# NUMERIC FUNCTIONS
+
+# max() --> gives the maximum number in the list present or given as input
+# example : print(max(1,2,3,4,5))
+# min() --> gives the minimum number
+# abs() --> find the absolute value of input  -ve to +ve for integers and float || for complex numbers gives magnitude 
+#  pow(_, _) --> give inputs as base and power 
+
+
+
+
