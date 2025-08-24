561ad0fd611ba49031321eb4379428223d986fbe
Sachin Kumar Jangir <127860904+SachinJangirX@users.noreply.github.com>
2025-08-24 22:33:14 +0530

    Added new files

---
+++ Controll Structure in Python.py
@@ -0,0 +1,62 @@
+
+
+# relative operators 
+# >, < , >=, <=, ==, !=,     
+
+
+
+# if-else statements
+
+# if(test_expression):
+#     statement
+# elif(text_expression):
+#     statement
+# else:
+#     statement
+
+
+## QUESTION TO TAKE INPUTS AND GIVE THE ASKED OPERATOR VALUES 
+# a = int(input('Enter Operand1: '))
+# b = int(input('Enter Operand2: '))
+# operator = input('Enter operator(+,-,*,/): ')
+
+# if(operator == '+'):
+#     print(a+b)
+# elif(operator == '-'):
+#     print(a-b)
+# elif(operator == '*'):
+#     print(a*b)
+# elif(operator == '/'):
+#     print(a/b)
+# else:
+#     print('!!!Enter the correct operator!!!')
+
+#  RANGE FUNCTION
+
+# n = list(range(10))    --> this one creates a list of 10 values with start = 0 and end = 9
+#  o/p -> [0,1,2,3,4,5,6,7,8,9]
+
+# we could also give the start value as list(range(2,8)) --> [2,3,4,5,6,7] 
+# general formula = list(range(start_value, end_value, step_size))
+
+
+# FOR LOOPS 
+
+# l = [list of iteams || could be of diff like int, str, float, double, bool]
+# for x in l:               x -> variable name for each item        l -> name of the list
+#     print(x)
+
+
+# WHILE LOOPS
+
+# while(txt_expression):
+#     statement
+#     increament/decreament
+
+
+# counter = 1
+# while(counter<6):
+#     print('a')
+#     counter+=1
+
+# if we are working with the decrementing ones then pick the higher value of counter and then decrease it till it matches with false ones
